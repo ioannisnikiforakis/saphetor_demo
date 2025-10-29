6f8fe957a7bc4b01270e404011a286a46e1ec954
Ioannis Nikiforakis <ioannis.nikiforakis@gmail.com>
2025-10-29 15:27:49 +0200

    Corrected Celery dependency

--- set_dependencies.sh
+++ set_dependencies.sh
@@ -2,6 +2,6 @@ python3 -m pip install Django
 pip3 install djangorestframework
 pip3 install django-filter
 pip3 install djangorestframework-xml
-pip3 install celery django-celery
+pip install celery[redis] django
 pip3 install vcfpy
 pip3 install supervisor
